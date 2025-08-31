cf1e69fbae344420aa499c63008e305f757be8b4
OlegOleksenko <oleg.oleksenko@gmail.com>
2025-08-31 16:59:22 +0300

    impementation HallButtons.h

--- lib/obp60task/HallButtons.h
+++ lib/obp60task/HallButtons.h
@@ -7,5 +7,42 @@
 
 class HallButtons {
 public:
+  typedef void (*ButtonCallback)(int index);
+  typedef void (*CombinationCallback)(uint16_t mask);
 
-}
+  HallButtons(const int *pins, int numButtons, int treshhold = 80, float adaptRate = 0.005, float filterAlpha = 0.02);
+
+  void begin();
+
+  void setPressCallback(ButtonCallback cb);
+  void setReleaseCallback(ButtonCallback cb);
+  void setShortPressCallback(ButtonCallback cb);
+  void setLongPressCallback(ButtonCallback cb);
+  void setCombinationCallback(CombinationCallback cb, uint16_t mask);
+
+  bool isPressed(int index);
+
+private:
+  static void readTask(void *pvParameters);
+  void readButtons();
+
+  const int *_pins;
+  int _numButtons;
+  int _threshold;
+  float _adaptRate;
+  float _filterAlpha;
+
+  float *_baseline;
+  float *_filtered;
+  float *_buttonState;
+  unsigned long *_pressStart;
+
+  ButtonCallback pressCallback = nullptr;
+  ButtonCallback releaseCallback = nullptr;
+  ButtonCallback shortPressCallback = nullptr;
+  ButtonCallback longPressCallback = nullptr;
+  CombinationCallback combinationCallback = nullptr;
+  uint16_t combinationMask = 0;
+
+  static HallButtons *instance;
+};
